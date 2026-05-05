32c2679e3d935026c9796699f6c8fcc30b792fc3
Rafael Lastre <lastrerafael4@gmail.com>
2026-05-05 12:18:43 -0700

    patch

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+import eventlet
+eventlet.monkey_patch()
+
 from flask import Flask, render_template, request, redirect, url_for, session
 from flask_socketio import SocketIO, emit
 from werkzeug.security import generate_password_hash, check_password_hash
