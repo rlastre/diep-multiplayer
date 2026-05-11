bdf752e667c8bb46078c0653e58c36e2a197863c
RyanMHsiao <109319635+RyanMHsiao@users.noreply.github.com>
2026-05-11 15:20:33 -0700

    Pull from main

--- app.py
+++ app.py
@@ -199,7 +199,7 @@ def on_pickup(data):
     item_id = data.get('item_id')
     if item_id in items:
         del items[item_id]
-        newScale = min(2.4, players[request.sid]['scale'] + 0.2 / players[request.sid]['scale'])
+        newScale = min(2.4, players[request.sid]['scale'] + 0.3 / players[request.sid]['scale'])
         if request.sid in players:
             players[request.sid]['scale'] = newScale
         socketio.emit('item_picked', {'item_id': item_id, 'player_id': request.sid, 'scale': newScale})
